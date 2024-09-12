f9c088b22b03dae0cc175e2061f34c734cbaf568
wassim <wassimzenasi@gmail.com>
2024-09-12 17:12:24 +0200

    opdr 1 af

--- opdracht/S1_DDL_student.sql
+++ opdracht/S1_DDL_student.sql
@@ -6,88 +6,67 @@
 -- Tijmen Muller (tijmen.muller@hu.nl)
 -- André Donk (andre.donk@hu.nl)
 --
---
 -- Opdracht: schrijf SQL-queries om onderstaande resultaten op te vragen,
 -- aan te maken, verwijderen of aan te passen in de database van de
 -- bedrijfscasus.
---
--- Codeer je uitwerking onder de regel 'DROP VIEW ...' (bij een SELECT)
--- of boven de regel 'ON CONFLICT DO NOTHING;' (bij een INSERT)
--- Je kunt deze eigen query selecteren en los uitvoeren, en wijzigen tot
--- je tevreden bent.
---
--- Vervolgens kun je je uitwerkingen testen door de testregels
--- (met [TEST] erachter) te activeren (haal hiervoor de commentaartekens
--- weg) en vervolgens het hele bestand uit te voeren. Hiervoor moet je de
--- testsuite in de database hebben geladen (bedrijf_postgresql_test.sql).
--- NB: niet alle opdrachten hebben testregels.
---
--- Lever je werk pas in op Canvas als alle tests slagen. Draai daarna
--- alle wijzigingen in de database terug met de queries helemaal onderaan.
 -- ------------------------------------------------------------------------
 
+-- S1.1 Geslacht
+ALTER TABLE medewerkers
+    ADD COLUMN geslacht CHAR(1);
 
--- S1.1. Geslacht
---
--- Voeg een kolom `geslacht` toe aan de medewerkerstabel.
--- Voeg ook een beperkingsregel `m_geslacht_chk` toe aan deze kolom,
--- die ervoor zorgt dat alleen 'M' of 'V' als geldige waarde wordt
--- geaccepteerd. Test deze regel en neem de gegooide foutmelding op als
--- commentaar in de uitwerking.
-
-
--- S1.2. Nieuwe afdeling
---
--- Het bedrijf krijgt een nieuwe onderzoeksafdeling 'ONDERZOEK' in Zwolle.
--- Om de onderzoeksafdeling op te zetten en daarna te leiden wordt de
--- nieuwe medewerker A DONK aangenomen. Hij krijgt medewerkersnummer 8000
--- en valt direct onder de directeur.
--- Voeg de nieuwe afdeling en de nieuwe medewerker toe aan de database.
+ALTER TABLE medewerkers
+    ADD CONSTRAINT m_geslacht_chk
+        CHECK (geslacht IN ('M', 'V'));
 
 
--- S1.3. Verbetering op afdelingentabel
---
--- We gaan een aantal verbeteringen doorvoeren aan de tabel `afdelingen`:
---   a) Maak een sequence die afdelingsnummers genereert. Denk aan de beperking
---      dat afdelingsnummers veelvouden van 10 zijn.
---   b) Voeg een aantal afdelingen toe aan de tabel, maak daarbij gebruik van
---      de nieuwe sequence.
---   c) Op enig moment gaat het mis. De betreffende kolommen zijn te klein voor
---      nummers van 3 cijfers. Los dit probleem op.
+-- S1.2 Nieuwe afdeling 'ONDERZOEK' en medewerker A DONK toevoegen
+INSERT INTO afdelingen (anr, aname, locatie)
+VALUES (50, 'ONDERZOEK', 'ZWOLLE');
 
+INSERT INTO medewerkers (mnr, naam, voorl, functie, chef, afd, gbdatum, maandsal)
+VALUES (8000, 'DONK', 'A', 'ONDERZOEKER', NULL, 50, '1970-01-01', 3000);
 
--- S1.4. Adressen
---
--- Maak een tabel `adressen`, waarin de adressen van de medewerkers worden
--- opgeslagen (inclusief adreshistorie). De tabel bestaat uit onderstaande
--- kolommen. Voeg minimaal één rij met adresgegevens van A DONK toe.
---
---    postcode      PK, bestaande uit 6 karakters (4 cijfers en 2 letters)
---    huisnummer    PK
---    ingangsdatum  PK
---    einddatum     moet na de ingangsdatum liggen
---    telefoon      10 cijfers, uniek
---    med_mnr       FK, verplicht
+-- S1.3 Verbeteringen aan de afdelingen-tabel
+-- a) Maak een sequence voor afdelingsnummers
+CREATE SEQUENCE afdelingen_seq
+    START WITH 60
+    INCREMENT BY 10;
 
+-- b) Voeg nieuwe afdelingen toe
+INSERT INTO afdelingen (anr, aname, locatie)
+VALUES (nextval('afdelingen_seq'), 'HR', 'UTRECHT');
 
--- S1.5. Commissie
---
--- De commissie van een medewerker (kolom `comm`) moet een bedrag bevatten als de medewerker een functie als
--- 'VERKOPER' heeft, anders moet de commissie NULL zijn. Schrijf hiervoor een beperkingsregel. Gebruik onderstaande
--- 'illegale' INSERTs om je beperkingsregel te controleren.
+INSERT INTO afdelingen (anr, aname, locatie)
+VALUES (nextval('afdelingen_seq'), 'FINANCE', 'AMSTERDAM');
 
-INSERT INTO medewerkers (mnr, naam, voorl, functie, chef, gbdatum, maandsal, comm)
-VALUES (8001, 'MULLER', 'TJ', 'TRAINER', 7566, '1982-08-18', 2000, 500);
+-- c) Kolom aanpassen als nummers te klein zijn
+ALTER TABLE afdelingen
+ALTER COLUMN anr TYPE INTEGER;
 
-INSERT INTO medewerkers (mnr, naam, voorl, functie, chef, gbdatum, maandsal, comm)
-VALUES (8002, 'JANSEN', 'M', 'VERKOPER', 7698, '1981-07-17', 1000, NULL);
+-- S1.4 Tabel adressen maken
+CREATE TABLE adressen (
+                          postcode CHAR(6),
+                          huisnummer INTEGER,
+                          ingangsdatum DATE,
+                          einddatum DATE CHECK (einddatum > ingangsdatum),
+                          telefoon CHAR(10) UNIQUE,
+                          med_mnr INTEGER,
+                          PRIMARY KEY (postcode, huisnummer, ingangsdatum),
+                          FOREIGN KEY (med_mnr) REFERENCES medewerkers(mnr)
+);
 
+-- Voeg een rij met adresgegevens van A DONK toe
+INSERT INTO adressen (postcode, huisnummer, ingangsdatum, einddatum, telefoon, med_mnr)
+VALUES ('1234AB', 1, '2020-01-01', NULL, '0612345678', 8000);
+
+-- S1.5 Commissie beperkingsregel
+ALTER TABLE medewerkers
+    ADD CONSTRAINT comm_verkoper_chk
+        CHECK ((functie = 'VERKOPER' AND comm IS NOT NULL) OR (functie != 'VERKOPER' AND comm IS NULL));
 
 
 -- -------------------------[ HU TESTRAAMWERK ]--------------------------------
--- Met onderstaande query kun je je code testen. Zie bovenaan dit bestand
--- voor uitleg.
-
 SELECT * FROM test_exists('S1.1', 1) AS resultaat
 UNION
 SELECT * FROM test_exists('S1.2', 1) AS resultaat
@@ -98,7 +77,6 @@ SELECT * FROM test_exists('S1.4', 6) AS resultaat
 UNION
 SELECT 'S1.5 wordt niet getest: handmatige test beschikbaar.' AS resultaat
 ORDER BY resultaat;
-
 
 -- Draai alle wijzigingen terug om conflicten in komende opdrachten te voorkomen.
 DROP TABLE IF EXISTS adressen;
