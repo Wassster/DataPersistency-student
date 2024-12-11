3f84150ceec36c5c0a635be6bfbb3d578a38c7fd
wassim <wassimzenasi@gmail.com>
2024-12-11 19:26:12 +0100

    iets

--- opdracht/S6_Views_student.sql
+++ opdracht/S6_Views_student.sql
@@ -18,6 +18,19 @@
 --       FROM medewerkers;
 -- 3. Is de view "deelnemers" updatable ? Waarom ?
 
+DROP VIEW IF EXISTS deelnemers;
+CREATE OR REPLACE VIEW deelnemers AS
+SELECT i.cursist, i.cursus, i.begindatum, u.docent, u.locatie
+FROM inschrijvingen i
+JOIN uitvoeringen u ON i.cursus = u.cursus AND i.begindatum = u.begindatum;
+
+-- Query om de "deelnemers" view te combineren met "personeels" view
+SELECT d.cursist, d.cursus, p.voorl, p.medewerker, d.locatie
+FROM deelnemers d
+JOIN personeel p ON d.cursist = p.mnr;
+
+-- Antwoord op vraag 3: De view "deelnemers" is niet updatable omdat deze een join bevat, en wijzigingen in een view met meerdere tabellen kunnen niet eenduidig naar een specifieke bron worden doorgevoerd.
+
 
 -- S6.2.
 --
@@ -27,3 +40,18 @@
 --    Deze view dient de uitvoeringsgegevens op te halen voor de "dagcurssussen" (gebruik ook de view "dagcursussen"). Toon aan dat de view werkt
 -- 3. Verwijder de views en laat zien wat de verschillen zijn bij DROP view <viewnaam> CASCADE en bij DROP view <viewnaam> RESTRICT
 
+DROP VIEW IF EXISTS dagcursussen;
+CREATE OR REPLACE VIEW dagcursussen AS
+SELECT c.code, c.omschrijving, c.type
+FROM cursussen c
+WHERE LENGTH(c.type) = 1;
+
+DROP VIEW IF EXISTS daguitvoeringen;
+CREATE OR REPLACE VIEW daguitvoeringen AS
+SELECT u.cursus, u.begindatum, u.docent, u.locatie
+FROM uitvoeringen u
+JOIN dagcursussen d ON u.cursus = d.code;
+
+-- Demonstratie van verschillen tussen DROP CASCADE en RESTRICT
+-- DROP VIEW dagcursussen CASCADE; -- Hiermee worden "dagcursussen" én afhankelijke views zoals "daguitvoeringen" verwijderd
+-- DROP VIEW dagcursussen RESTRICT; -- Dit faalt als er afhankelijke views zijn, zoals "daguitvoeringen"
